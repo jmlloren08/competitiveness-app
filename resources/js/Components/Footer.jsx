774928adca162df5e43d9ec9038d29f12ee8a62e
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2024-07-17 05:24:08 -0400

    add about us page and footer

--- resources/js/Components/Footer.jsx
+++ resources/js/Components/Footer.jsx
@@ -4,8 +4,9 @@ import { faFacebook, faInstagram, faTwitter, faYoutube } from '@fortawesome/free
 
 export default function Footer() {
     return (
-        <footer className="bg-gray-100 w-full pt-10 pb-5">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-4 gap-8">
+        <footer className="w-full m-0 p-0">
+            {/* main footer */}
+            <div className="bg-gray-100 max-w-7xl m-auto p-4 sm:p-6 lg:p-8 grid grid-cols-1 md:grid-cols-4 gap-8">
                 <div>
                     <h3 className="text-lg font-bold mb-2">MAIN OFFICE ADDRESS</h3>
                     <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'>4th & 5th Floor, NFA Building, NFA Compound, Visayas Avenue, Brgy. Vasra, Diliman, Quezon City, Philippines 1128</p>
@@ -42,8 +43,33 @@ export default function Footer() {
                     </div>
                 </div>
             </div>
-            <div className="w-full px-4 sm:px-6 lg:px-8 mt-8 text-center">
-                <p className="text-sm">&copy; {new Date().getFullYear()} Republic of the Philippines. All rights reserved.</p>
+            {/* sub footer */}
+            <div className="bg-gray-200 max-w-7xl m-auto p-4 sm:p-6 lg:p-8 grid grid-cols-1 md:grid-cols-4 gap-8">
+                <div>
+                    <img src="https://www.officialgazette.gov.ph/wp-content/themes/govph/assets/images/footlogo.png" alt="GOV Logo" className='h-48' />
+                </div>
+                <div>
+                    <h1 className="text-lg font-bold mb-2">REPUBLIC OF THE PHILIPPINES</h1>
+                    <p>All content is in the public domain unless otherwise stated.</p>
+                </div>
+                <div>
+                    <h1 className="text-lg font-bold mb-2">ABOUT GOVPH</h1>
+                    <p>Learn more about the Philippine government, its structure, how government works and the people behind it.</p>
+                    <p className='m-1 p-1'></p>
+                    <p>GOV.PH</p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="https://gov.ph/data">Open Data Portal</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="https://officialgazette.gov.ph">Official Gazette</a></p>
+                </div>
+                <div>
+                    <h1 className="text-lg font-bold mb-2">GOVERNMENT LINKS</h1>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="https://president.gov.ph">Office of the President</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="https://www.ovp.gov.ph">Office of the Vice President</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="http://www.senate.gov.ph">Senate of the Philippines</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="http://www.congress.gov.ph">House of Representatives</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="http://sc.judiciary.gov.ph">Supreme Court</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="http://ca.judiciary.gov.ph/">Court of Appeals</a></p>
+                    <p className='border-transparent text-gray-500 hover:text-red-700 hover:border-gray-300 focus:text-gray-700 focus:border-gray-300'><a href="http://sb.judiciary.gov.ph/">Sandiganbayan</a></p>
+                </div>
             </div>
         </footer>
     );
--- resources/js/Pages/ReportPage.jsx
+++ resources/js/Pages/ReportPage.jsx
@@ -1,13 +1,89 @@
-import { Head, Link } from '@inertiajs/react';
+import { Head } from '@inertiajs/react';
 import React from 'react';
 
 const ReportPage = () => {
     return (
         <>
             <Head title="Competitiveness Dashboard - Reports" />
-            <div>
-                <h1>Welcome, Reports</h1>
-                <p>back to... <Link href={route('/')}>Home</Link></p>
+
+            <div className="min-h-screen bg-white-100">
+                <nav className="bg-white border-b border-gray-100">
+                    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div className="flex justify-between items-center h-24">
+
+                            <NavBar />
+
+                            <div className="-mr-2 flex items-center sm:hidden">
+                                <button
+                                    onClick={() => setShowingNavigationDropdown((previousState) => !previousState)}
+                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
+                                >
+                                    <svg className="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                                        <path
+                                            className={!showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M4 6h16M4 12h16M4 18h16"
+                                        />
+                                        <path
+                                            className={showingNavigationDropdown ? 'inline-flex' : 'hidden'}
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            strokeWidth="2"
+                                            d="M6 18L18 6M6 6l12 12"
+                                        />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={(showingNavigationDropdown ? 'block' : 'hidden') + ' sm:hidden'}>
+                        <div className="pt-2 pb-3 space-y-1">
+                            <ResponsiveNavLink href={route('/')}>
+                                Home
+                            </ResponsiveNavLink>
+                        </div>
+
+                        <div className="pt-4 pb-1 border-t border-gray-200">
+
+                            <div className="px-4">
+                                {/* <div className="font-medium text-base text-gray-800">{user.name}</div>
+                            <div className="font-medium text-sm text-gray-500">{user.email}</div> */}
+                            </div>
+
+                            <div className="mt-3 space-y-1">
+                                <ResponsiveNavLink href={route('about')}>About</ResponsiveNavLink>
+                                <ResponsiveNavLink href={route('reports')} active={route().current('reports')}>
+                                    Reports
+                                </ResponsiveNavLink>
+                                <ResponsiveNavLink href={route('news')}>News</ResponsiveNavLink>
+                                <ResponsiveNavLink href={route('contact-us')}>Contact Us</ResponsiveNavLink>
+                            </div>
+
+                        </div>
+                    </div>
+                </nav>
+
+                <main className='bg-white mx-auto py-6 px-4 sm:px-6 lg:px-8 mt-6 mb-6'>
+
+                    <section>
+                        <div className='max-w-6xl mx-auto shadow-lg p-6 rounded'>
+                            <h2 className='text-2xl font-bold mb-4 bg-sky-900 text-white p-5 rounded'>ABOUT THE REPORT CARD</h2>
+                            <p className='p-3'>
+                                The Report Card presents the rankings of the Philippines in major international competitiveness reports. The key objective of this collaboration is the development of a comprehensive “competitiveness dashboard”.
+                            </p>
+                            <p className='p-3'>
+                                This dashboard will serve as an integrated platform designed to monitor and analyze various aspects of the country’s competitiveness. It will offer a centralized view of key data and performance metrics, enabling stakeholders to make informed decisions and implement strategies for enhancing competitiveness.
+                            </p>
+                        </div>
+                    </section>
+
+                </main>
+
+                <Footer />
+
             </div>
         </>
     )
