c923a7422600306ee1603d6bc2621a4663e7462b
jmlloren08 <65296830+jmlloren08@users.noreply.github.com>
2024-07-18 22:20:50 +0800

    add menu and submenu icon

--- resources/js/Pages/ReportPage.jsx
+++ resources/js/Pages/ReportPage.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import NavBar from '@/Components/NavBar';
 import Footer from '@/Components/Footer';
-import { Head } from '@inertiajs/react';
+import { Head, Link } from '@inertiajs/react';
 
 export default function ReportPage() {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -11,52 +11,119 @@ export default function ReportPage() {
     const menuItems = [
         {
             name: 'General', icon: '/backend/assets/images/icon-general.png',
-            subMenu: [{name: 'World Competitiveness Yearbook', icon: '/backend/assets/images/icon-wcy.png'}]
-        },
-        {
-            name: 'Digital', icon: '/backend/assets/image/icon-digital.png',
             subMenu: [
-                'Digital Quality of Life Index', icon: '/backend/assets/images/dqli.png',
-                'GovTech Maturity Index', icon: '/backend/assets/images/gtmi.png',
-                'ICT Development Index', icon: '/backend/assets/images/ictdi.png'
-                'E-Government Development Index',
-                'Network Readiness Index',
-                'World Digital Competitiveness Ranking'
+                {
+                    name: 'World Competitiveness Yearbook',
+                    icon: '/backend/assets/images/icon-wcy.png'
+                }
             ]
         },
         {
-            name: 'Education Talent & Innovation',
+            name: 'Digital', icon: '/backend/assets/images/icon-digital.png',
             subMenu: [
-                'Programme for International Student Assessment (PISA)',
-                'Global Innovation Index',
-                'Global Talent Competitiveness Index',
-                'World Talent Ranking'
+                {
+                    name: 'Digital Quality of Life Index',
+                    icon: '/backend/assets/images/icon-dqli.png'
+                },
+                {
+                    name: 'GovTech Maturity Index',
+                    icon: '/backend/assets/images/icon-gtmi.png'
+                },
+                {
+                    name: 'ICT Development Index',
+                    icon: '/backend/assets/images/icon-ictdi.png'
+                },
+                {
+                    name: 'E-Government Development Index',
+                    icon: '/backend/assets/images/icon-egdi.png'
+                },
+                {
+                    name: 'Network Readiness Index',
+                    icon: '/backend/assets/images/icon-nri.png'
+                },
+                {
+                    name: 'World Digital Competitiveness Ranking',
+                    icon: '/backend/assets/images/icon-wdcr.png'
+                }
             ]
         },
         {
-            name: 'Energy & Environment',
+            name: 'Education Talent & Innovation', icon: '/backend/assets/images/icon-eti.png',
             subMenu: [
-                'Energy Transition Index (ETI)',
-                'Environmental Performance Index'
+                {
+                    name: 'Programme for International Student Assessment (PISA)',
+                    icon: '/backend/assets/images/icon-pisa.png'
+                },
+                {
+                    name: 'Global Innovation Index',
+                    icon: '/backend/assets/images/icon-gii.png'
+                },
+                {
+                    name: 'Global Talent Competitiveness Index',
+                    icon: '/backend/assets/images/icon-gtci.png'
+                },
+                {
+                    name: 'World Talent Ranking',
+                    icon: '/backend/assets/images/icon-wtr.png'
+                }
             ]
         },
         {
-            name: 'Governance',
+            name: 'Energy & Environment', icon: '/backend/assets/images/icon-ee.png',
             subMenu: [
-                'B-Ready/Doing Business Report',
-                'Corruption Perceptions Index',
-                'Economic Freedom Index',
-                'Worldwide Governance Indicators'
+                {
+                    name: 'Energy Transition Index (ETI)',
+                    icon: '/backend/assets/images/icon-eti.png'
+                },
+                {
+                    name: 'Environmental Performance Index',
+                    icon: '/backend/assets/images/icon-epi.png'
+                }
             ]
         },
         {
-            name: 'Health & Nutrition',
+            name: 'Governance', icon: '/backend/assets/images/icon-governance.png',
             subMenu: [
-                'Global Health Security Index',
-                'Global Hunger Index'
+                {
+                    name: 'B-Ready/Doing Business Report',
+                    icon: '/backend/assets/images/icon-brdbr.png'
+                },
+                {
+                    name: 'Corruption Perceptions Index',
+                    icon: '/backend/assets/images/icon-cpi.png'
+                },
+                {
+                    name: 'Economic Freedom Index',
+                    icon: '/backend/assets/images/icon-efi.png'
+                },
+                {
+                    name: 'Worldwide Governance Indicators',
+                    icon: '/backend/assets/images/icon-wgi.png'
+                }
             ]
         },
-        { name: 'Trade', subMenu: ['Logistics Performance Index'] }
+        {
+            name: 'Health & Nutrition', icon: '/backend/assets/images/icon-hn.png',
+            subMenu: [
+                {
+                    name: 'Global Health Security Index',
+                    icon: '/backend/assets/images/icon-ghsi.png'
+                },
+                {
+                    name: 'Global Hunger Index',
+                    icon: '/backend/assets/images/icon-ghi.png'
+                }
+            ]
+        },
+        {
+            name: 'Trade', icon: '/backend/assets/images/icon-trade.png',
+            subMenu: [
+                {
+                    name: 'Logistics Performance Index',
+                    icon: '/backend/assets/images/icon-lpi.png'
+                }
+            ]
+        }
     ]
 
     const handleMenuClick = (menu) => {
@@ -66,6 +133,8 @@ export default function ReportPage() {
     const handleBackClick = () => {
         setSelectedMenu('');
     }
+
+    const selectedMenuItem = menuItems.find(item => item.name === selectedMenu);
 
     return (
         <>
@@ -142,20 +211,20 @@ export default function ReportPage() {
                     </div>
                 </header>
 
-                <main className='bg-white mx-24 py-6 px-4 sm:px-6 lg:px-8 mt-6 mb-12'>
+                <main className='bg-white mx-32 py-6 px-4 sm:px-6 lg:px-8 mt-6 mb-12'>
 
                     <section className='text-center'>
                         {!selectedMenu ? (
-                            <div className='grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7'>
+                            <div className='grid grid-rows-7 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4 sm:gap-0 md:gap-0 lg:gap-0'>
                                 {menuItems.map((item, index) => (
                                     <div key={index} className="relative">
                                         <button
-                                            className='bg-blue-900 text-white font-bold py-4 px-6 rounded-lg transition-transform transform hover:scale-105 w-10/12 h-72'
+                                            className='bg-gradient-to-r from-blue-900 to-blue-950 text-white font-bold py-4 px-6 rounded-lg transition-transform transform hover:scale-105 w-10/12 sm:w-11/12 h-64 sm:h-72'
                                             onClick={() => handleMenuClick(item.name)}
                                         >
                                             <div>
                                                 <img
-                                                    src={`/backend/assets/images/${item.name.toLowerCase()}.png`}
+                                                    src={item.icon}
                                                     alt={`${item.name} Icon`}
                                                     className='h-auto mx-auto'
                                                     style={{ filter: 'invert(100%)' }}
@@ -167,17 +236,33 @@ export default function ReportPage() {
                                 ))}
                             </div>
                         ) : (
-                            <div className="mb-6 bg-blue-900 text-white font-bold py-4 px-6 rounded-lg">
+                            <div className="mb-6 bg-gradient-to-r from-blue-950 to-blue-900 text-white font-bold py-4 px-6 rounded-lg">
+                                <div className="mb-4">
+                                    <img
+                                        src={selectedMenuItem.icon}
+                                        alt={`${selectedMenu} Icon`}
+                                        className='h-16 mx-auto'
+                                        style={{ filter: 'invert(100%)' }}
+                                    />
+                                </div>
                                 <h2 className="text-2xl font-bold mb-4">{selectedMenu}</h2>
-                                <ul>
-                                    {menuItems.find(item => item.name === selectedMenu).subMenu.map((subItem, subIndex) => (
-                                        <li key={subIndex} className="text-lg mb-2">
-                                            {subItem}
-                                        </li>
+                                <div className="grid grid-rows-1 sm:grid-rows-auto gap-4 flex items-center justify-center">
+                                    {selectedMenuItem.subMenu.map((subItem, subIndex) => (
+                                        <div key={subIndex} className="no-bg hover:bg-black transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 text-white font-bold py-2 px-4 rounded-lg flex items-center">
+                                            <div>
+                                                <img
+                                                    src={subItem.icon}
+                                                    alt={`${subItem.name} Icon`}
+                                                    className='h-12 mx-auto'
+                                                    style={{ filter: 'invert(100%)' }}
+                                                />
+                                            </div>
+                                            <div className="no-underline hover:underline text-lg mx-3 flex items-center"><Link>{subItem.name}</Link></div>
+                                        </div>
                                     ))}
-                                </ul>
+                                </div>
                                 <button
-                                    className='mt-4 bg-gray-800 text-white font-bold py-2 px-4 rounded-lg'
+                                    className='mt-4 bg-gray-800 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 text-white font-bold py-2 px-4 rounded-lg'
                                     onClick={handleBackClick}
                                 >
                                     Back to Main Menu
